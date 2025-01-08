14dbb33139416b3e984195daf2b97158196ca2d5
Mario <kucek.mario@gmail.com>
2025-01-08 22:22:31 +0100

    End of section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class ActivityStore{
     selectedActivity: Activity | null | undefined = null;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor(){
         makeAutoObservable(this)
@@ -19,12 +19,12 @@ export default class ActivityStore{
     }
 
     loadActivities = async () => {
+        this.setLoadingInitial(true);
     try{
         const activities = await agent.Activities.list();
         
         activities.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        this.activityRegistry.set(activity.id, activity);
+            this.setActivity(activity);
         })
         this.setLoadingInitial(false);
     } catch (error) {
@@ -33,26 +33,40 @@ export default class ActivityStore{
     
     }
 }
+
+loadActivity = async (id: string) => {
+    let activity = this.getActivity(id);
+    if(activity){
+        this.selectedActivity = activity;
+        return activity;
+    } else {
+        this.loadingInitial = true;
+        try{
+            activity = await agent.Activities.details(id);
+            runInAction(() => {
+                this.selectedActivity = activity;
+                this.loadingInitial = false;
+                return activity;
+            })
+            return activity;
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loadingInitial = false;
+            })
+        }
+    }
+}
+private setActivity = (activity: Activity) => {
+    activity.date = activity.date.split('T')[0];
+    this.activityRegistry.set(activity.id, activity);
+}
+private getActivity = (id: string) => {
+    return this.activityRegistry.get(id);
+}
+
 setLoadingInitial = (state: boolean) => {
     this.loadingInitial = state;
-}
-
-selectActivity = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
-}
-
-cancelSelectedActivity = () => {
-    this.selectedActivity = undefined;
-}
-
-openForm = (id?: string) => {
-    id ? this.selectActivity(id) : this.cancelSelectedActivity();
-    this.editMode = true;
-}
-
-
-closeForm = () => {
-    this.editMode = false;
 }
 
 createActivity = async (activity: Activity) => {
@@ -100,7 +114,6 @@ deleteActivity = async (id: string) => {
         await agent.Activities.delete(id);
         runInAction(() => {
             this.activityRegistry.delete(id);
-            if(this.selectedActivity?.id === id) this.cancelSelectedActivity();
             this.loading = false;
         })
     } catch (error) {
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
+import { router } from './app/router/Routes'
 import { store, StoreContext } from './app/stores/store.ts'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StoreContext.Provider value={store}>
-      <App />
+    <RouterProvider router={router}/>
     </StoreContext.Provider>
   
   </StrictMode>,
